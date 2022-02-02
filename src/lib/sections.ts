a92044fdd027443d9dfd5a6d769310dc3ac03f46
Aidan Gerber <agentaidan@gmail.com>
2022-02-01 19:36:19 -0600

    large content updates

--- src/lib/sections.ts
+++ src/lib/sections.ts
@@ -1,18 +1,18 @@
 let course_schedule = [
-    { date_string: 'August 23, 2022', plan: 'Introduce general course ideas, talk about ETFs and Indexes (S&P, Nasdaq, Dow, Nikkei, etc.). Assign students to look at a company they have personally used and think about why it would be a good investment' },
+    { date_string: 'August 23, 2022', plan: 'Introduce general course ideas, talk about ETFs and Indexes (S&P, Nasdaq, Dow, Nikkei, etc.). What sources are good and what guidelines are there for finding new ones. Assign students to look at a company they have personally used and think about why it would be a good investment' },
     { date_string: 'August 30, 2022', plan: 'Fundamental Analysis Day 1 (determine value using macro and microeconomic factors like interest rates and profit trends)' },
     { date_string: 'September 6, 2022', plan: 'Fundamental Day 2' },
-    { date_string: 'September 13, 2022', plan: 'Fundamental Day 3' },
+    { date_string: 'September 13, 2022', plan: 'Guest Speaker 1' },
     { date_string: 'September 20, 2022', plan: 'Technical Analysis Day 1 (determine value using price, volume, and chart patterns)' },
     { date_string: 'September 27, 2022', plan: 'Technical Day 2' },
-    { date_string: 'October 4, 2022', plan: 'Technical Day 3' },
-    { date_string: 'October 11, 2022', plan: 'Derivative Day 1' },
-    { date_string: 'October 18, 2022', plan: 'Derivatives Day 2' },
-    { date_string: 'October 25, 2022', plan: 'Derivatives Day 3' },
-    { date_string: 'November 1, 2022', plan: 'Non-stock investments (real estate, crypto, commodities, etc.) and introduce final project' },
+    { date_string: 'October 4, 2022', plan: 'Flex day' },
+    { date_string: 'October 11, 2022', plan: 'NO CLASS: Midterm Recess' },
+    { date_string: 'October 18, 2022', plan: 'Derivatives Day 1' },
+    { date_string: 'October 25, 2022', plan: 'Derivatives Day 2' },
+    { date_string: 'November 1, 2022', plan: 'Guest Speaker 2 and introduce final project' },
     { date_string: 'November 8, 2022', plan: 'Portfolio Building Day 1' },
     { date_string: 'November 15, 2022', plan: 'Portfolio Building Day 2' },
-    { date_string: 'November 22, 2022', plan: 'Portfolio Building Day 3' },
+    { date_string: 'November 22, 2022', plan: 'Non-stock investments (real estate, crypto, gold, commodities, gourd futures, etc.)' },
     { date_string: 'November 29, 2022', plan: 'Small Group Final Presentations' },
 ]
 
@@ -23,16 +23,16 @@ for (let course of course_schedule) {
 let schedule_string = ''
 
 for (let schedule of course_schedule) {
-    schedule_string = schedule_string.concat(`<h2>${schedule.date_string}</h2><p>${schedule.plan}</p>`)
+    schedule_string = schedule_string.concat(`<h3>${schedule.date_string}</h3><p>${schedule.plan}</p>`)
 }
 
 const course_description = `The recent GameStop short squeeze, staggering market performance
 since the COVID-19 pandemic, rising inflation, and the booming housing market have brought investing 
 to the forefront of public attention. These factors alter the risk and reward balance that 
 each individual considers when they invest their money with the larger question of 
-<strong>how can investing tools and data help an individual achieve their financial goals.</strong>
-In Data-Driven Investing, students will learn the fundamentals of investing and apply them in a live stock market competition. 
-Together, we'll look at tough investing questions and formulate opinions with data and critical analysis.
+<strong>how can investing tools and evidence help an individual achieve their financial goals.</strong>
+In Evidence-based Investing, students will learn the fundamentals of investing and apply them in a live stock market competition. 
+Together, we'll look at tough investing questions and formulate opinions with evidence and critical analysis.
 With these skills, we'll analyze the claims made by prominent investors and perform original research — 
 rather than blindly listening to Reddit posts with 8 upvotes.
 The course contains no exams — instead, students complete a portfolio strategy pitch of their choice. By taking this
@@ -42,25 +42,23 @@ const course_objective = `By the end of this course, students will understand th
 the basic steps of picking a company and analyzing it on to concepts like diversification and risk management.
 `
 
-const learning_objectives = `1. Evaluate stocks using fundamental and technical analysis.<br>
-2. Interpret whether a stock is worth purchasing and whether it fits within a given strategy.<br>
-3. Analyze investing claims from a variety of sources using data to determine accuracy.<br>
-4. Summarize investing concepts for others`
+const learning_objectives = `<ol><li>Evaluate stocks using fundamental and technical analysis.</li>
+<li>Interpret whether a stock is worth purchasing and whether it fits within a given strategy.</li>
+<li>Analyze investing claims from a variety of sources using evidence to determine accuracy.</li>
+<li>Summarize investing concepts for others</li></ol>`
 
 let sections = [
-    { heading: 'Data-Driven Investing', content: '', order: 1 },
-    { heading: 'Contact Information', content: 'Instructor: Aidan Gerber<br>URL: <a href="https://syllabus.pages.dev/">https://syllabus.pages.dev/</a><br>Email: gerber@rice.edu', order: 2 },
+    { heading: 'Evidence-based Investing', content: '', order: 1 },
+    { heading: 'Contact Information', content: 'Instructor: Aidan Gerber<br>URL: <a href="https://syllabus.pages.dev/">https://syllabus.pages.dev/</a><br>Email: gerber@rice.edu<br>Office Hours: By appointment', order: 2 },
     { heading: 'Course Description', content: course_description, order: 3 },
     { heading: 'Learning Objectives', content: learning_objectives, order: 3.1 },
-    { heading: 'Assessments', content: ' In periods of 3 weeks each, students will learn fundamental and technical analysis, equity derivatives, and portfolio review. After learning these concepts, small groups will present a concept to the class in a discussion-based environment. At the end of the semester, students will pitch a group of stocks based on what they learned.', order: 3.2 },
-    { heading: 'Course Objectives and Learning Outcomes', content: course_objective, order: 4 },
-    { heading: 'Required Texts and Materials', content: 'This course has no required materials', order: 5 },
+    { heading: 'Assessments', content: ' In periods of 2 weeks each, students will learn fundamental and technical analysis, equity derivatives, and portfolio review. After learning these concepts, individuals or pairs will present a concept to half the class in a discussion-based environment. At the end of the semester, students will pitch a group of stocks based on what they learned. There will also be a paper trading competition run through the semester.', order: 3.2 },
+    { heading: 'Learning Outcomes', content: course_objective, order: 4 },
+    { heading: 'Required Texts and Materials', content: 'This course has no required materials. We will be using tools like FRED, Yahoo Finance, Capital IQ, Koyfin, Quiverquant, etc.', order: 5 },
     { heading: 'Exams and Papers', content: 'Instead of exams or papers, this class will contain a final project. Students will design and pitch a portfolio of equities based on strategies they learned in the class.', order: 6 },
-    { heading: 'Grade Policies', content: 'Participation: 50%<br>Topic Presentations: 35%<br>Final Project: 15%', order: 7 },
-    { heading: 'Absence Policies', content: 'Attending this class is important. If you know you are going to be absent, let the instructor know and class content will be provided.', order: 8 },
-    {
-        heading: 'Rice Honor Code', content: `In this course, all students will be held to the standards of the Rice Honor Code, a code that you pledged to honor when you matriculated at this institution. If you are unfamiliar with the details of this code and how it is administered, you should consult the Honor System Handbook at <a href="http://honor.rice.edu/honor-system-handbook/">http://honor.rice.edu/honor-system-handbook/</a>. This handbook outlines the University's expectations for the integrity of your academic work, the procedures for resolving alleged violations of those expectations, and the rights and responsibilities of students and faculty members throughout the process.`, order: 9
-    },
+    { heading: 'Grade Policies', content: 'Participation: 40%<br>Topic Presentations: 40%<br>Final Project: 20%. Satisfactory/unsatisfactory cutoff: 65%', order: 7 },
+    { heading: 'Absence Policies', content: 'Attending this class is important. If you know you are going to be absent, let the instructor know and class content will be provided. The class also has two unexcused absences.', order: 8 },
+    { heading: 'Rice Honor Code', content: `In this course, all students will be held to the standards of the Rice Honor Code, a code that you pledged to honor when you matriculated at this institution. If you are unfamiliar with the details of this code and how it is administered, you should consult the Honor System Handbook at <a href="http://honor.rice.edu/honor-system-handbook/">http://honor.rice.edu/honor-system-handbook/</a>. This handbook outlines the University's expectations for the integrity of your academic work, the procedures for resolving alleged violations of those expectations, and the rights and responsibilities of students and faculty members throughout the process.`, order: 9 },
     { heading: 'Disability Resource Center', content: 'If you have a documented disability or other condition that may affect academic performance you should: 1) make sure this documentation is on file with the Disability Resource Center (Allen Center, Room 111 / adarice@rice.edu / x5841) to determine the accommodations you need; and 2) talk with me to discuss your accommodation needs.', order: 10 },
     { heading: 'Syllabus Change Policy', content: 'This syllabus is only a guide for the course and is subject to change with advanced notice.', order: 11 },
     { heading: 'Course Schedule', content: schedule_string, order: 11 },
